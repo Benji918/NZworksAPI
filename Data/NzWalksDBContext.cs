7764da349172f1537c41cd32706e188d688baf38
Chukwukodinaka Ugochukwu <kodiugos@gmail.com>
2026-08-23 15:22:44 +0100

    refactor: added dbcontext and DB connection string to the application

---
+++ Data/NzWalksDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZworks.Models.Domain;
+
+namespace NZworks.Data
+{
+    public class NzWalksDBContext : DbContext
+    {
+        public NzWalksDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
