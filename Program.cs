e4368ee8912b04bb001b6a27994132dc9878176f
Chukwukodinaka Ugochukwu <kodiugos@gmail.com>
2026-09-18 00:21:32 +0100

    chore: added config for adding authentication in the project

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using NZworks.Data;
 using NZworks.Repositories;
 using NZworks.Mappings;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 var builder = WebApplication.CreateBuilder(args);
 
 //Add http logging
@@ -34,6 +35,22 @@ builder.Services.AddAutoMapper(cfg =>
     cfg.AddProfile<AutoMapperProfiles>();
 
 });
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Auth0:Domain"],
+            ValidAudience = builder.Configuration["Auth0:Audience"]
+        };
+        options.Authority = builder.Configuration["Auth0:Domain"];
+        options.Audience = builder.Configuration["Auth0:Audience"];
+    });
+
 
 
 var app = builder.Build();
